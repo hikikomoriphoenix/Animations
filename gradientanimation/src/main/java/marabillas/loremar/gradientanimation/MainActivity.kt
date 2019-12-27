c32d9b47610e43ca12cf188f6a5e2dd30174a180
Loremar Marabillas <loremar5858@gmail.com>
2019-12-27 17:38:43 +0800

    Gradient Animation

--- gradientanimation/src/main/java/marabillas/loremar/gradientanimation/MainActivity.kt
+++ gradientanimation/src/main/java/marabillas/loremar/gradientanimation/MainActivity.kt
@@ -19,10 +19,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        val view = findViewById<View>(R.id.gradient_view)
+        view1Animation()
+        view2Animation()
+    }
+
+    private fun view1Animation() {
+        val view = findViewById<View>(R.id.view1)
         val drawable = view.background as GradientDrawable
         val color1 = ResourcesCompat.getColor(resources, R.color.colorPrimary, null)
-        val color2 = ResourcesCompat.getColor(resources, R.color.colorPrimaryDark, null)
+        val color2 = ResourcesCompat.getColor(resources, R.color.colorAccent, null)
 
         val end = 320 * resources.displayMetrics.density
         val evaluator = ArgbEvaluator()
@@ -50,6 +55,45 @@ class MainActivity : AppCompatActivity() {
         AnimatorSet().apply {
             duration = 5000
             playSequentially(first, second)
+            doOnEnd { start() }
+        }.start()
+    }
+
+    private fun view2Animation() {
+        val view2 = findViewById<View>(R.id.view2)
+        val color1 = ResourcesCompat.getColor(resources, R.color.colorPrimary, null)
+        val color2 = ResourcesCompat.getColor(resources, R.color.colorAccent, null)
+        val evaluator = ArgbEvaluator()
+
+        val gradientBackground = GradientDrawable().apply {
+            gradientType = GradientDrawable.LINEAR_GRADIENT
+            orientation = GradientDrawable.Orientation.LEFT_RIGHT
+            colors = intArrayOf(color1, color2)
+            view2.background = this
+        }
+
+        fun gradientAnimator(startColor: Int, endColor: Int, isStartPhase: Boolean) =
+            ValueAnimator.ofFloat(0f, 1f).apply {
+                addUpdateListener {
+                    val value = it.animatedValue as Float
+                    gradientBackground.apply {
+                        val transformColor = evaluator.evaluate(value, endColor, startColor) as Int
+                        colors = if (isStartPhase)
+                            intArrayOf(transformColor, endColor)
+                        else
+                            intArrayOf(startColor, transformColor)
+                    }
+                }
+            }
+
+        val phase1 = gradientAnimator(color1, color2, true)
+        val phase2 = gradientAnimator(color1, color2, false)
+        val phase3 = gradientAnimator(color2, color1, true)
+        val phase4 = gradientAnimator(color2, color1, false)
+
+        AnimatorSet().apply {
+            duration = 1000
+            playSequentially(phase1, phase2, phase3, phase4)
             doOnEnd { start() }
         }.start()
     }
